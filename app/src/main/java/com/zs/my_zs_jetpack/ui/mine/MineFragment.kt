fc1ec07ba402ff684448aa10b0cc8af3604b07cd
梁培源 <liangpeiyuan@yingzi.com>
2025-08-20 11:33:29 +0800

    完善EventBus

--- app/src/main/java/com/zs/my_zs_jetpack/ui/mine/MineFragment.kt
+++ app/src/main/java/com/zs/my_zs_jetpack/ui/mine/MineFragment.kt
@@ -11,10 +11,20 @@ import androidx.navigation.fragment.findNavController
 import com.zs.my_zs_jetpack.R
 import com.zs.my_zs_jetpack.common_base.LazyBaseFragment
 import com.zs.my_zs_jetpack.databinding.FragmentMineBinding
+import com.zs.my_zs_jetpack.event.LoginEvent
+import com.zs.my_zs_jetpack.event.LogoutEvent
 import com.zs.my_zs_jetpack.extension.clickNoRepeat
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 class MineFragment : LazyBaseFragment<FragmentMineBinding>() {
     private val mineVm by viewModels<MineViewModel>()
+
+    override fun init() {
+        EventBus.getDefault().register(this)
+    }
+
     override fun lazyInit() {
         binding.vm = mineVm
         mineVm.getInternalBean()
@@ -25,9 +35,25 @@ class MineFragment : LazyBaseFragment<FragmentMineBinding>() {
             findNavController().navigate(R.id.action_mainFragment_to_loginFragment)
         }
         binding.ivHead.clickNoRepeat {
-            mineVm.getInternalBean()
+//            mineVm.getInternalBean()
+            mineVm.resetIntegralBean()
         }
 
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public fun loginEvent(loginEvent: LoginEvent) {
+        mineVm.getInternalBean()
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public fun logoutEvent(logoutEvent: LogoutEvent) {
+        mineVm.resetIntegralBean()
+    }
+
+    override fun onDestroy() {
+        EventBus.getDefault().unregister(this)
+        super.onDestroy()
     }
 
     override fun getLayoutId(): Int = R.layout.fragment_mine
