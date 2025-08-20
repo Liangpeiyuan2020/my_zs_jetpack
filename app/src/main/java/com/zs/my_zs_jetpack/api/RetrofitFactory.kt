f622c56d394f722b7f613bd911754c708e58314d
梁培源 <liangpeiyuan@yingzi.com>
2025-08-20 10:47:30 +0800

    解决cookie的问题

--- app/src/main/java/com/zs/my_zs_jetpack/api/RetrofitFactory.kt
+++ app/src/main/java/com/zs/my_zs_jetpack/api/RetrofitFactory.kt
@@ -1,37 +1,61 @@
 package com.zs.my_zs_jetpack.api
 
+//import com.franmontiel.persistentcookiejar.ClearableCookieJar
+//import com.franmontiel.persistentcookiejar.PersistentCookieJar
+//import com.franmontiel.persistentcookiejar.cache.SetCookieCache
+//import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor
+import com.zs.my_zs_jetpack.constants.Constants
+//import okhttp3.Cache
+//import okhttp3.Interceptor
+//import okhttp3.OkHttpClient
+//import retrofit2.Retrofit
+//import retrofit2.converter.gson.GsonConverterFactory
+//import java.io.File
+//import java.security.AccessController.getContext
+//import java.util.concurrent.TimeUnit
+//import java.util.logging.Level
+import com.zs.my_zs_jetpack.common.http.HttpLoggingInterceptor
+
+
+import com.franmontiel.persistentcookiejar.ClearableCookieJar
+import com.franmontiel.persistentcookiejar.PersistentCookieJar
+import com.franmontiel.persistentcookiejar.cache.SetCookieCache
+import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor
+import com.zs.my_zs_jetpack.utils.BaseApp.Companion.getContext
+//import com.zs.base_library.BaseApp.Companion.getContext
+//import com.zs.base_library.http.HttpLoggingInterceptor
+//import com.zs.zs_jetpack.constants.ApiConstants
+//import com.zs.zs_jetpack.constants.Constants
 import okhttp3.Cache
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.File
-import java.security.AccessController.getContext
 import java.util.concurrent.TimeUnit
 import java.util.logging.Level
-
 object RetrofitFactory {
     private val BASE_URL: String = "https://www.wanandroid.com"
-    private val DEFAULT_TIMEOUT = 10000
     private val okHttpClientBuilder: OkHttpClient.Builder
         get() {
             return OkHttpClient.Builder()
                 .readTimeout(
-                    DEFAULT_TIMEOUT.toLong(),
+                    Constants.DEFAULT_TIMEOUT.toLong(),
                     TimeUnit.MILLISECONDS
                 )
                 .connectTimeout(
-                    DEFAULT_TIMEOUT.toLong(),
+                    Constants.DEFAULT_TIMEOUT.toLong(),
                     TimeUnit.MILLISECONDS
                 )
                 .addInterceptor(getLogInterceptor())
-
+                .cookieJar(getCookie())
+                .cache(getCache())
         }
 
     fun getFactory(): Retrofit {
         val okHttpClient = okHttpClientBuilder.build()
-        return Retrofit.Builder().client(okHttpClient)
+        return Retrofit.Builder()
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(BASE_URL)
             .build()
@@ -42,28 +66,29 @@ object RetrofitFactory {
      */
     private fun getLogInterceptor(): Interceptor {
         //http log 拦截器
-        val logging = HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BASIC
+        return HttpLoggingInterceptor("OkHttp").apply {
+            setPrintLevel(HttpLoggingInterceptor.Level.BODY)
+            setColorLevel(Level.INFO)
         }
-        return logging
     }
 
-//    /**
-//     * 获取cookie持久化
-//     */
-//    private fun getCookie():ClearableCookieJar{
-//        return PersistentCookieJar(
-//            SetCookieCache(),
-//            SharedPrefsCookiePersistor(getContext())
-//        )
-//    }
+    /**
+     * 获取cookie持久化
+     */
+    private fun getCookie(): ClearableCookieJar {
+        return PersistentCookieJar(
+            SetCookieCache(),
+            SharedPrefsCookiePersistor(getContext())
+        )
+    }
 
     /**
      * 获取缓存方式
      */
-//    private fun getCache():Cache{
-//        //缓存100Mb
-//        return Cache( File(getContext().cacheDir, "cache")
-//            , 1024 * 1024 * 100)
-//    }
+    private fun getCache(): Cache {
+        //缓存100Mb
+        return Cache(
+            File(getContext().cacheDir, "cache"), 1024 * 1024 * 100
+        )
+    }
 }
